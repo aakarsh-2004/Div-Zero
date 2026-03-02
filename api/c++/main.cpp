ec4f69c301cd3002aa342c8bee7b28bb35e23db3
aakarsh-2004 <aakarshbeohar2004@gmail.com>
2026-03-02 16:03:49 +0530

    add missing import of config

--- api/c++/main.cpp
+++ api/c++/main.cpp
@@ -5,12 +5,7 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    for(auto& x: a) cin >> x;
-
-    cout << *max_element(a.begin(), a.end()) << endl;
+    cout << "hello\n";
     
     return 0;
 }
